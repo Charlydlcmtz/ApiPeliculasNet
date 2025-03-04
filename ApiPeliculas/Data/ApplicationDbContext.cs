0f8e536f0c4aaeb043087cda1a270c6a331de51a
Cdelacruz <Cdelacruz@TC-SIS-DES-2-P>
2025-03-04 17:17:31 -0600

    Agregar archivos de proyecto.

---
+++ ApiPeliculas/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiPeliculas.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPeliculas.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //Aqui se pasan todos las entidades (modelos)
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
